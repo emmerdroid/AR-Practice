ee68b62c84c7b9d84c8f4b76a8ebb9b2fef30003
emmerdroid <58455108+emmerdroid@users.noreply.github.com>
2022-09-08 16:31:57 -0700

    Created script to work on the detection for each of the layers in the 3d environment

---
+++ Assets/Manny Scripts/PlanetLayer Detection.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Description of how code should work
+/*
+ * Each layer prefab will have a collider
+ * The colliders will check to see if they are touching with another prefab that follows along in correct order
+ * If so then create a prefab that will have both materials, viewing it in a cross section faction
+ *      Will have to make prefab for 2 layer, 3 layer, 4 layer 
+ * If not then indicate that the user has done it wrong. 
+ * Order of layers will be Outer Crust -> Inner crust -> Mantle -> Core -> Inner Core
+ */
+public class PlanetLayerDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //get what THIS game object name is/ what layer it is
+    //What comes before/after
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == )
+        {
+
+        }
+    }
+}
