da18e4c9c83140fa6bc367b6361f1050d93fcbdc
emmerdroid <58455108+emmerdroid@users.noreply.github.com>
2022-09-15 22:22:14 -0700

    Fixed Drag/Slide issues. Worked on the Material changer script
    Material drag reads objects and has all materials needed to adjust as wanted, currently has a loop that will activate at start to change all materials in the object to the clear material, but unsure if it will work with crust object as it has 2 layers.

--- Assets/Manny Scripts/MaterialChanger.cs
+++ Assets/Manny Scripts/MaterialChanger.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class MaterialChanger : MonoBehaviour
 {
+    //Layers ad their element in the object
+    /*
+     * Element 0 -> Clear Material
+     * Element 1 -> Core
+     * Element 2 -> Crust 
+     * Element 3 -> Crust Out
+     * Element 4 -> Inner Core
+     * Element 5 -> Mantle
+     */
 
-
-    [SerializeField] Material[] layers;
-    [SerializeField] GameObject[] layerObj;
+    [SerializeField] Material[] layers; //materials to use
+    [SerializeField] GameObject[] layerObj; // actual layer objects in the prefab
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < this.transform.childCount; i++)
         {
             layerObj[i] = this.transform.GetChild(i).gameObject;
+
+        }
+
+        for(int i = 0; i < layerObj.Length; i++)
+        {
+            layerObj[i].gameObject.GetComponent<Renderer>().material = layers[i];
+            //in complete, crust layer has 2 materials so will need to check to see 
+            //if this works with that or if needs adjustment
 
         }
     }
