194c2c55fc6d0ad74c2038f65c7dfde26ca051d6
emmerdroid <58455108+emmerdroid@users.noreply.github.com>
2023-02-17 10:22:26 -0800

    Got the Earth to Work with image tracking

---
+++ Assets/ViewChanger.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewChanger : MonoBehaviour
+{
+
+    public GameObject OuterView;
+    public GameObject InnerView;
+
+    Button viewButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        viewButton = FindObjectOfType<Button>();
+        viewButton.onClick.AddListener(ChangeView);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeView()
+    {
+
+        Debug.Log("Swap");
+        Debug.Log("Outer view is " + OuterView.activeSelf);
+        Debug.Log("Inner view is " + InnerView.activeSelf);
+        // when pressed, the 2 views switch options
+       
+        OuterView.SetActive(!OuterView.activeSelf);
+        InnerView.SetActive(!InnerView.activeSelf);
+
+
+        
+    }
+}
