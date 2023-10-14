3bdfff21129d171f5b3b537735790e6e0d935210
emmerdroid <58455108+emmerdroid@users.noreply.github.com>
2023-10-13 18:30:55 -0700

    Working on scripting out progress and timer to questions

--- Assets/Manny Scripts/Scripts For AR Game/ProgressBar.cs
+++ Assets/Manny Scripts/Scripts For AR Game/ProgressBar.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
+    public int Max; //max for number of questions
+    [SerializeField] int current; //current question the player is on.
+    public Image mask;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,12 @@ public class ProgressBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetCurrentFill();
+    }
+
+    void GetCurrentFill()
+    {
+        float fillAmount = (float)current / (float)Max;
+        mask.fillAmount = fillAmount;
     }
 }
---
+++ Assets/Manny Scripts/Timer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+
+    public int  MaxTime;
+    [SerializeField] int timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
