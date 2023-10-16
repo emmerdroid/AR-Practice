19dfa2efc0ea19c5993c3787c4e38a53c2032802
emmerdroid <58455108+emmerdroid@users.noreply.github.com>
2023-10-15 20:11:01 -0700

    Timer now works, need to work on game with questions and answers

--- Assets/Manny Scripts/Timer.cs
+++ Assets/Manny Scripts/Timer.cs
@@ -1,21 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
 
     public int  MaxTime;
-    [SerializeField] int timer;
+    [SerializeField] float timer;
+    [SerializeField]Image timerBar;
+    public bool timerRunning = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = MaxTime;
+        timerRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(timerRunning) 
+        {
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                timerBar.fillAmount = timer / MaxTime;
+            }
+            else
+            {
+                Debug.Log("Time ran out.");
+                timerRunning = false;
+            }
+        }
         
     }
 }
