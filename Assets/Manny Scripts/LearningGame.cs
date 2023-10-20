7853044c67c47d90c01e93f872ab45dfd5352f32
Emmanuel Salcedo <58455108+emmerdroid@users.noreply.github.com>
2023-10-20 09:47:50 -0700

    Update on Game

--- Assets/Manny Scripts/LearningGame.cs
+++ Assets/Manny Scripts/LearningGame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,7 +12,10 @@ public class LearningGame : MonoBehaviour
     public Dictionary<string, string> questions_answers;
     public Button[] answers; //0-3, 0 is top left, 1 top right,
                              //2 bottom left, 3 bottom right
-    public Text question;
+    public ProgressBar progressBar;
+    public TMP_Text question;
+    int questionPos;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -19,15 +23,16 @@ public class LearningGame : MonoBehaviour
         questions_answers = new Dictionary<string, string>()
         {
             {"What layer is 1.400 miles thick?", "Outer Core" },
-            {"What layer is 9,000° F?" , "Outer Core"},
-            {"What layer is 9,800° F?" ,"Inner Core"},
+            {"What layer is 9,000ï¿½ F?" , "Outer Core"},
+            {"What layer is 9,800ï¿½ F?" ,"Inner Core"},
             {"Which layer is sandwhiched between the crust and outer core?", "Mantle"},
             {"Which layer is the thickest?","Mantle"},
             {"What layer goes about 19 miles deep on average?","Crust"},
             {"What layer has the consistency of caramel?","Mantle"}
         };
 
-        question.text = questions_answers.ElementAt(0).Key;
+        questionPos = 0;
+        question.text = questions_answers.ElementAt(questionPos).Key;
 
 
 
@@ -43,9 +48,28 @@ public class LearningGame : MonoBehaviour
         while (timer.timerRunning) 
         {
             //the game is going
+            //wait and listen to buttons.
+            // if the person gets it correct, pause/reset the timer
+            // if incorrect show the correct answer and allow them to move on.
+
+            foreach (Button b in answers)
+            {
+                int correct = Random.Range(0, answers.Length);
+                answers[correct].transform.GetComponentInChildren<TextMeshProUGUI>().text = questions_answers.ElementAt(questionPos).Value;
+
+
+            }
 
         }
 
         //randomly assign answers to the buttons
+    }
+    
+
+    //Button functionality
+
+    void Next(){
+        questionPos = questionPos + 1;
+        question.text = questions_answers.ElementAt(questionPos).Key;
     }
 }
